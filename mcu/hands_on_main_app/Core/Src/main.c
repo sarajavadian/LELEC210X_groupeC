02192fc15c14e83741c0ad8b661b9f166f11450e
Tom Hanssens <tom.hanssens@student.uclouvain.be>
2024-03-04 14:40:40 +0100

    Main app mode

--- mcu/hands_on_main_app/Core/Src/main.c
+++ mcu/hands_on_main_app/Core/Src/main.c
@@ -213,6 +213,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
@@ -225,6 +226,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
@@ -280,5 +282,3 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
